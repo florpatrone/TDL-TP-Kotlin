635a564d560d0d9802c2e99a07c9febab774fa11
Matias <Cabralmatiasezequiel@gmail.com>
2021-01-30 11:17:20 -0300

    clase director pregunta

--- src/builders/DirectorPregunta.kt
+++ src/builders/DirectorPregunta.kt
@@ -1,4 +1,22 @@
 package builders
 
-class DirectorPregunta {
+class DirectorPregunta(var tipoPuntaje: String, var enunciado: String, opciones: List<OpcionSerializada?>) {
+    var opciones: List<OpcionSerializada?>
+
+    constructor(preguntaSerializada: PreguntaSerializada) : this(
+        preguntaSerializada.getPuntajeType(),
+        preguntaSerializada.getEnunciado(),
+        preguntaSerializada.getOptions()
+    ) {
+    }
+
+    fun asignar(builder: Builder) {
+        builder.setOpciones(opciones)
+        builder.asignarComportamiento(tipoPuntaje)
+        builder.setEnunciado(enunciado)
+    }
+
+    init {
+        this.opciones = opciones
+    }
 }

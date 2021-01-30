bb794c32656dc03d7f0e92031e71a4f203bd8aee
ngaido <nicolasgaido1@gmail.com>
2021-01-30 15:34:52 -0300

    Copy paste nuevo

--- src/model/Juego.kt
+++ src/model/Juego.kt
@@ -2,41 +2,68 @@ package model
 
 import java.util.ArrayList
 
-class Juego {
-
+class Juego : Observable {
     private var jugadorInicial: Jugador? = null
     var jugadorActual: Jugador? = null
-    private var preguntaActual: Pregunta? = null
-
-    fun empezarJuego(nombreJugador1: String, nombreJugador2: String) {
-        val jugador1 = Jugador(nombreJugador1)
-        val jugador2 = Jugador(nombreJugador2)
-        jugador1.jugadorSiguiente = jugador2
-        jugador2.jugadorSiguiente = jugador1
+        private set
+    var preguntaActual: Pregunta
+        private set
+    var exclusividadActual: Exclusividad? = null
+    private val observers: ArrayList<Observer>
+    fun empezarJuego(nombreJugador1: String?, nombreJugador2: String?) {
+        val jugador1 = Jugador(nombreJugador1!!)
+        val jugador2 = Jugador(nombreJugador2!!)
+        jugador1.setJugadorSiguiente(jugador2)
+        jugador2.setJugadorSiguiente(jugador1)
         jugadorInicial = jugador1
         jugadorActual = jugadorInicial
+        exclusividadActual = SinExclusividad()
+        notifyObservers()
     }
 
-    private fun elegirRespuestasAPreguntaActual(respuestasElegidas: ArrayList<Opcion>): Jugador? {
-        jugadorActual?.elegirRespuestasAPreguntaActual(respuestasElegidas)
-        return jugadorActual?.jugadorSiguiente
+    private fun elegirRespuestasAPreguntaActual(respuestasElegidas: ArrayList<Opcion>): Jugador {
+        jugadorActual!!.elegirRespuestasAPreguntaActual(respuestasElegidas)
+        return jugadorActual.getJugadorSiguiente()
     }
 
     fun siguienteTurno(respuestasElegidas: ArrayList<Opcion>) {
         jugadorActual = elegirRespuestasAPreguntaActual(respuestasElegidas)
         if (jugadorActual === jugadorInicial) {
-            preguntaActual = preguntaActual?.siguientePregunta
+            exclusividadActual.definirPuntosJugadoresEnPregunta(preguntaActual, jugadorActual)
+            preguntaActual = preguntaActual.getSiguientePregunta()
+            exclusividadActual = SinExclusividad()
         }
+        notifyObservers()
     }
 
-    fun getPreguntaActual(): Pregunta? {
-        return preguntaActual
+    fun activarExclusividad() {
+        exclusividadActual = preguntaActual.activarExclusividad(exclusividadActual)
     }
 
-    val tipoPregunta: String?
-        get() = preguntaActual?.tipoPregunta
+    fun activarMultiplicadorX2() {
+        preguntaActual.agregarMultiplicadorAJugador(jugadorActual, MultiplicadorX2())
+    }
+
+    fun activarMultiplicadorX3() {
+        preguntaActual.agregarMultiplicadorAJugador(jugadorActual, MultiplicadorX3())
+    }
+
+    val tipoPregunta: String
+        get() = preguntaActual.getTipoPregunta()
+
+    fun addObserver(observer: Observer) {
+        observers.add(observer)
+    }
+
+    fun notifyObservers() {
+        observers.forEach(Observer::update)
+    }
 
     val ganador: Jugador?
-        get() = if (jugadorActual?.puntos!! > jugadorActual?.jugadorSiguiente?.puntos!!) jugadorActual else jugadorActual!!.jugadorSiguiente
+        get() = if (jugadorActual.getPuntos() > jugadorActual.getJugadorSiguiente().getPuntos()) jugadorActual else jugadorActual.getJugadorSiguiente()
 
+    init {
+        preguntaActual = Parser().parsear()
+        observers = ArrayList<Observer>()
+    }
 }
--- src/model/Jugador.kt
+++ src/model/Jugador.kt
@@ -1,25 +1,27 @@
 package model
 
-import java.util.*
 import model.modificadores.Multiplicador
+import java.util.*
 
 class Jugador(val nombre: String) {
-
     var puntos = 0
-    private var respuestasElegidas: ArrayList<Opcion>
+        private set
+    private var multiplicadorClase: Multiplicador
+    var respuestasElegidas: ArrayList<Opcion>
+        private set
     var jugadorSiguiente: Jugador? = null
-    private var multiplicadorClase: Multiplicador? = null
 
     fun elegirOpciones(respuestasElegidas: ArrayList<Opcion>) {
         this.respuestasElegidas = respuestasElegidas
     }
 
-    fun setMultiplicador(multiplicadorClase: Multiplicador?) {
+    fun setMultiplicador(multiplicadorClase: Multiplicador) {
         this.multiplicadorClase = multiplicadorClase
     }
 
     fun ganarPuntaje(puntaje: Int) {
-        puntos += puntaje
+        puntos += multiplicadorClase.modificarPuntos(puntaje)
+        setMultiplicador(MultiplicadorBasico())
     }
 
     fun procesarPregunta(pregunta: Pregunta, respuestas: List<Opcion?>?) {
@@ -30,13 +32,8 @@ class Jugador(val nombre: String) {
         this.respuestasElegidas = respuestasElegidas
     }
 
-    fun getRespuestasElegidas(): ArrayList<Opcion> {
-        return respuestasElegidas
-    }
-
     init {
-        respuestasElegidas = ArrayList<Opcion>()
+        respuestasElegidas = ArrayList()
+        multiplicadorClase = MultiplicadorBasico()
     }
-
-
 }

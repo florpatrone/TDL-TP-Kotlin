3621ed5ea2d0f8c7361231bceef2378e96b622ee
Fabián Colque <fabian.colque@hotmail.com>
2021-01-30 11:58:02 -0300

    Se agrega paquete data el cual contiene las serializaciones de Opcion y Pregunta

---
+++ src/data/OpcionSerializada.kt
@@ -0,0 +1,22 @@
+package data
+
+class OpcionSerializada {
+
+    //@SerializedName("clave")
+    //@Expose
+    private val clave: String? = null
+
+    //@SerializedName("texto")
+    //@Expose
+    private val texto: String? = null
+
+    fun getClave(): String? {
+        return clave
+    }
+
+
+    fun getTexto(): String? {
+        return texto
+    }
+}
+
